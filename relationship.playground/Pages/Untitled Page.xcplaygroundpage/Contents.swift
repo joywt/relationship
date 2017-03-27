207190ac72d68c00512748b8abc8f6ef0d23b177
wangtie <tiewang3399@gmail.com>
2017-03-27 20:03:39 +0800

    修改语法

--- relationship.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ relationship.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -2,62 +2,70 @@
 
 import UIKit
 
-
-
-
-
-
-
-
-
 /*
  关系】f:父,m:母,h:夫,w:妻,s:子,d:女,xb:兄弟,ob:兄,lb:弟,xs:姐妹,os:姐,ls:妹
  
  【修饰符】 &o:年长,&l:年幼,#:隔断,[a|b]:并列
  */
 
+
+// 算法分析
+/*
+ 
+ 1. 我的爸爸的妻子(f,w) = 妈妈(m)
+ 2. 我的姐姐的弟弟(os,lb) = 自己/弟弟/哥哥 ["","lb","ob"]
+ */
+
+
+
+
 // 返回称谓集合
-func chengweiData() -> Dictionary<String,Array<Any>>? {
-    let filePath = Bundle.main.path(forResource: "data", ofType: "json")
-    let fileUrl = URL.init(fileURLWithPath: filePath!, isDirectory: true)
-    let fileData = try? Data(contentsOf: fileUrl, options: [])
-    if let fileData = fileData {
-        let data = try? JSONSerialization.jsonObject(with: fileData, options: JSONSerialization.ReadingOptions.mutableContainers)
-        if let data = data as? Dictionary<String,Array<Any>> {
-            return data
+
+var dataSource:[String:[Any]]? {
+    if let filePath = Bundle.main.path(forResource: "data", ofType: "json"){
+        let fileUrl = URL(fileURLWithPath: filePath, isDirectory: true)
+        if let fileData = try? Data(contentsOf: fileUrl, options: []) {
+            if let data = try? JSONSerialization.jsonObject(with: fileData, options: JSONSerialization.ReadingOptions.mutableContainers) as? Dictionary<String,Array<Any>> {
+                return data
+            }
         }
-        
+        return nil
+    }else {
+        return nil
     }
-    return nil
+}
+let data = dataSource
+
+// 关系链检索集合
+
+var relationShipSource:[[String:String]]? {
+    if let filePath = Bundle.main.path(forResource: "filter", ofType: "json") {
+        let fileUrl = URL.init(fileURLWithPath: filePath, isDirectory: true)
+        if let fileData = try? Data(contentsOf: fileUrl, options: []){
+            if let data = try? JSONSerialization.jsonObject(with: fileData, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String:Any]{
+                if let filter = data?["filter"] as? Array<Dictionary<String,String>> {
+                    return filter
+                }
+            }
+            
+        }
+        return nil
+    }else {
+        return nil
+    }
 }
 
-// 关系链集合
-func relationship() -> Array<Dictionary<String,String>>? {
-    let filePath = Bundle.main.path(forResource: "filter", ofType: "json")
-    let fileUrl = URL.init(fileURLWithPath: filePath!, isDirectory: true)
-    let fileData = try? Data(contentsOf: fileUrl, options: [])
-    if let fileData = fileData {
-        let data:AnyObject! = try? JSONSerialization.jsonObject(with: fileData, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject!
-        if let data = data {
-            let filter:Array<Dictionary<String,String>> = data["filter"] as! Array<Dictionary<String, String>>
-            return filter
-        }
-        
-    }
-    return nil
-}
+let filter = relationShipSource
 
-let data = chengweiData()
-let filter = relationship()
+// 称谓转换成关联字符
 
 // 是否反转
 var reverse = false
-// 称谓转换成关联字符
-func transform(str:String) -> String? {
+
+func transformTitleToKey(str:String) -> String? {
     var result = String()
     if !str.isEmpty{
         var lists = str.components(separatedBy: "的")
-        //var match = true
         while lists.count > 0 {
             let name = lists.first
             lists.removeFirst()
@@ -100,109 +108,58 @@ struct RegexHelper {
     }
 }
 
-struct FilteHelper {
+class FilteHelper {
     var result: Array<String>
-    let filterData = relationship()
-    let selector: String
+    let filterData = relationShipSource
     var hash = Set<String>()
-    init(_ input: String) {
+    init() {
         result = Array()
-        selector = input
     }
-    mutating func filter(input:String){
-        var sel = input
-        var s = String()
-        if (!hash.contains(sel)){
-            hash.insert(sel)
-            if (sel.isEmpty){ // 我 自身直接添加不进行 filter
-                result.append(sel)
-            }else {
+    
+    func filt(input:String) {
+        var theInputStr = input
+        if !hash.contains(theInputStr) {
+            hash.insert(theInputStr)
+            if theInputStr.isEmpty { // "" 指 我，直接添加不进行检索
+                result.append(theInputStr)
+            }else{
+                var temp = String()
                 var status = true
-                repeat{
-                    s = sel
-                    if let temp = filterData {
-                        for item in temp {
-                            let exp = item["exp"]
-                            let str = item["str"]
-                            let replacer: RegexHelper = try! RegexHelper(exp!)
-                            sel = replacer.replace(input: sel, template: str!)
-                            print(sel)
-                            if sel.contains("#"){
-                                let sep = sel.components(separatedBy: "#")
-                                for key in sep {
-                                    //filterSelectors(selector: key)
+                repeat {
+                    temp = str
+                    if let fd = filterData {
+                        for item in fd {
+                            let exp:String = item["exp"] ?? ""
+                            let str:String = item["str"] ?? ""
+                            let replacer: RegexHelper = try! RegexHelper(exp)
+                            theInputStr = replacer.replace(input: theInputStr, template: str)
+                            print(theInputStr)
+                            
+                            if theInputStr.contains("#"){
+                                let sepSource = str.components(separatedBy: "#")
+                                for key in sepSource {
                                     print(key)
-                                    filter(input: key)
+                                    filt(input: key)
                                 }
                                 status = false
                                 break
                             }
                         }
                     }
-                }while s != sel
+                }while temp != str
                 
                 if (status){
-                    result.append(sel)
+                    result.append(theInputStr)
                 }
             }
-            
         }
-    }
-    
-    mutating func filterSelectors(){
-        filter(input:selector)
-    }
-}
-/*
-func filterSelectors(selector:String) -> Array<String>? {
-    var result = [String]()
-    var sel = filter(input: selector)
-    let sep = sel.components(separatedBy: "#")
-    if !sep.isEmpty{
-        for key in sep {
-            //filterSelectors(selector: key)
-            sel = filter(input: key)
-            result.append(key)
-        }
-    }else {
-        result.append(selector)
     }
 
-    //var hash = Dictionary<String,Bool>()
-        return result
-}*/
-
-func getData(d:String) -> Array<String>{
-    var res = Array<String>()
-    let filter =  "[olx]"
-    let replacer: RegexHelper = try! RegexHelper(filter)
-    if let theData = data {
-        for (key,values) in theData {
-            let newkey = replacer.replace(input: key, template: "")
-            if (newkey == d){
-                let value = values.first as! String
-                res.append(value)
-            }
-        }
-    }
-    
-    return res
 }
 
 
-    //return valueByKey(key: newkey)
-func valueByKey(key:String) -> String? {
-    
-    if let theData = data {
-        let values = theData[key]
-        if let values = values {
-            let value = values.first as! String
-            return value
-        }
-        
-    }
-    return nil
-}
+
+// 从数据源中查找对应 key 的结果
 
 func dataValueByKeys(result:Array<String>) -> String? {
     var dataValue = String()
@@ -270,6 +227,36 @@ func errorMessage(key:String) -> String{
     }
     return message
 }
+func getData(d:String) -> Array<String>{
+    var res = Array<String>()
+    let filter =  "[olx]"
+    let replacer: RegexHelper = try! RegexHelper(filter)
+    if let theData = data {
+        for (key,values) in theData {
+            let newkey = replacer.replace(input: key, template: "")
+            if (newkey == d){
+                let value = values.first as! String
+                res.append(value)
+            }
+        }
+    }
+    
+    return res
+}
+
+
+func valueByKey(key:String) -> String? {
+    
+    if let theData = data {
+        let values = theData[key]
+        if let values = values {
+            let value = values.first as! String
+            return value
+        }
+        
+    }
+    return nil
+}
 // 互查 反转
 
 func reverseKey(key:String) -> String {
@@ -305,16 +292,15 @@ func reverseKey(key:String) -> String {
 }
 
 func calculate(str:String) -> String {
-    var result = String()
-    let keys = transform(str: str)
-    
-    var filterHelper = FilteHelper(keys!)
-    filterHelper.filterSelectors()
-    result = dataValueByKeys(result: filterHelper.result)!
-    //filterHelper.result
-    //filterHelper.filter(input: ",ob")
-    //dataValueByKeys(result: [",s,d",",d"])
-    return result
+    if let keys = transformTitleToKey(str: str){
+        let filterHelper = FilteHelper()
+        filterHelper.filt(input: keys)
+        if let  result = dataValueByKeys(result: filterHelper.result){
+            return result
+        }
+        
+    }
+    return "计算错误"
 }
-var str  = "我的哥哥的妈妈的爸爸的妈妈的爸爸的妈妈的妈妈"
+var str  = "我的哥哥的妈妈的爸爸"
 calculate(str: str)
